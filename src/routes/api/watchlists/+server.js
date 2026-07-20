167ef3dde85dc69ae3a28c7c20abccfd7c51cfa0
uttamsaha546 <uttamsaha546@gmail.com>
2026-07-20 16:40:45 +0530

    m

--- src/routes/api/watchlists/+server.js
+++ src/routes/api/watchlists/+server.js
@@ -4,20 +4,20 @@ import { db } from "$lib/server/database";
 export function GET() {
     // 1. Fetch the raw rows from the database
     db.prepare(`INSERT OR REPLACE INTO watchlists (name, entries) VALUES (?, ?)`).run('Default Watchlist', 'VBL');
-    const rows = db.prepare(`SELECT * FROM watchlists`).all();
+    const watchlists = db.prepare(`SELECT * FROM watchlists`).all();
     
     const stockUniverse = db.prepare('SELECT symbol, name, marketcap FROM stock_universe').all();
     const myPortfolio = db.prepare(`SELECT holding FROM portfolio WHERE key='my_holding'`).get();
     
     const symbolMap = new Map(stockUniverse.map(x=>([x.symbol, x])));
-    const myPortfolioMap = new Map(JSON.parse(myPortfolio.holding).map(x=>([x.symbol, x.HoldingAmt])));
-    // // console.log(myPortfolioMap)
+    const myPortfolioHoldingMap = new Map(JSON.parse(myPortfolio.holding).map(x=>([x.symbol, x.HoldingAmt])));
+    console.log(myPortfolioHoldingMap)
 
     // // 2. Map through and parse the 'entries' string back into a JS array/object
-    const data = rows.map(row => ({
-        ...row,
-        entries: row.entries ? row.entries.split(",").map(symbol=>{
-          if(symbolMap.has(symbol)) return {...symbolMap.get(symbol), value: myPortfolioMap.has(symbol)? myPortfolioMap.get(symbol): null};
+    const data = watchlists.map(watchlist => ({
+        name:watchlist.name,
+        entries: watchlist.entries ? watchlist.entries.split(",").map(x=>x.trim()).map(symbol=>{
+          if(symbolMap.has(symbol)) return {...symbolMap.get(symbol), value: myPortfolioHoldingMap.has(symbol)? myPortfolioHoldingMap.get(symbol): null};
           return {symbol, name: null, marketcap: null, value: null}
         }
           ) : []
