3447cb62bc7adca13cce0dda4f8c60730b9e41e3
uttamsaha546 <uttamsaha546@gmail.com>
2026-07-20 23:49:37 +0530

    Update +page.svelte

--- src/routes/testPage/+page.svelte
+++ src/routes/testPage/+page.svelte
@@ -6,11 +6,20 @@
 	}
 </script>
 
-<div class="w-screen h-screen flex items-center justify-center">
+<div class="w-screen h-screen flex flex-col items-center justify-center gap-10">
 	<button
 		class="bg-indigo-600 text-white font-bold px-2 py-1 rounded hover:bg-indigo-700 cursor-pointer active:bg-indigo-700/80 shadow"
 		onclick={testButton}
 	>
 		Click to Test
 	</button>
+
+	<div class="bg-red-800 square">A</div>
 </div>
+
+<style>
+	.square {
+		width: 1in;
+		height: 1in;
+	}
+</style>
