36dfd83ad11ac753cd266bdb5925be6aa2e54355
uttamsaha546 <uttamsaha546@gmail.com>
2026-07-21 00:08:23 +0530

    Update +page.svelte

--- src/routes/chart/+page.svelte
+++ src/routes/chart/+page.svelte
@@ -58,6 +58,12 @@
 			window.isTouchCapable = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
 		}
 	});
+
+	let scalingMultiplier = $derived.by(() => {
+		if (!ChartState.isMonthly) return 1;
+		if (window.isTouchCapable) return 25;
+		return 30;
+	});
 </script>
 
 <div class="h-screen w-screen flex flex-col main-container">
@@ -67,7 +73,7 @@
 		<div
 			class="ChartArea h-full W-Flex-1 flex flex-1 min-w-0 cursor-crosshair active:cursor-grabbing relative"
 		>
-			<Chart data={ChartState.barData} scalingMultiplier={ChartState.isMonthly ? 30 : 1} />
+			<Chart data={ChartState.barData} {scalingMultiplier} />
 			<div
 				class="Timeframe absolute bottom-0 right-0 z-20 h-7 w-16 flex items-center justify-center"
 			>
